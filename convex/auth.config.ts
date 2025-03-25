4a72797acd6abf40f7e19d1b22f7fb86faf4b243
Pranjal2237 <90096515+Pranjal2237@users.noreply.github.com>
2025-03-25 10:54:04 +0530

    project completed

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://gorgeous-meerkat-7.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -0,0 +1,19 @@
+import { Zap } from "lucide-react";
+import Link from "next/link";
+
+export default function UpgradeButton() {
+  const CHEKOUT_URL =
+    "https://codecraft2237.lemonsqueezy.com/buy/92d85801-c4f1-4c9b-9cc2-0c924ac8c0ef";
+
+  return (
+    <Link
+      href={CHEKOUT_URL}
+      className="inline-flex items-center justify-center gap-2 px-8 py-4 text-white 
+        bg-gradient-to-r from-blue-500 to-blue-600 rounded-lg 
+        hover:from-blue-600 hover:to-blue-700 transition-all"
+    >
+      <Zap className="w-5 h-5" />
+      Upgrade to Pro
+    </Link>
+  );
+}
